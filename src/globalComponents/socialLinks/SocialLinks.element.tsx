939d9b2b5c0a44944b3738051e18aaf963a1a913
mmeurrens <milan.meurrens@hotmail.com>
2021-11-20 10:33:43 +0100

    Add social links

--- src/globalComponents/socialLinks/SocialLinks.element.tsx
+++ src/globalComponents/socialLinks/SocialLinks.element.tsx
@@ -4,10 +4,13 @@ import { Container, IconLink, Icon } from "./socialLinks.style";
 export const SocialLinks: React.FC<{}> = () => {
   return (
     <Container>
-      <IconLink href="https://google.com">
+      <IconLink href="https://github.com/MilanMeurrens" target="_blank">
         <Icon src={"/icons/github.svg"} alt="GitHub" />
       </IconLink>
-      <IconLink href="https://google.com">
+      <IconLink
+        href="https://www.linkedin.com/in/milanmeurrens/"
+        target="_blank"
+      >
         <Icon src={"/icons/linkedin.svg"} alt="LinkedIn" />
       </IconLink>
     </Container>
